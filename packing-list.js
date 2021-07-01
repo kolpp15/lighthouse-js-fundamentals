8c8d349c8cabc22cf2fe8c393bf4f496537c7073
Brian Sohn <kolpp15@gmail.com>
2021-06-30 22:25:17 -0700

    while looks are also possible to iterate over items in an array.

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,20 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//for loop example-----------------------------------
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+console.log("\n");
+
+//while loop can also be done -----------------------
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
+console.log("\n");
+
